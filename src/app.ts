d8a0869258bc570ec125c98b7979cb2f7d948e72
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2022-08-30 00:20:57 +0200

    Add support for CORS to the API

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import prisma from "./lib/prisma/client";
 
@@ -10,9 +11,15 @@ import {
     validationErrorMiddleware,
 } from "./lib/validation";
 
+const corsOptions = {
+    origin: "http://localhost:8080",
+};
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.get("/planets/:id(\\d+)", async (request, response, next) => {
     const planetId = Number(request.params.id);
--- src/server.test.ts
+++ src/server.test.ts
@@ -33,7 +33,8 @@ describe("GET /planets", () => {
         const response = await request
             .get("/planets")
             .expect(200)
-            .expect("Content-Type", /application\/json/);
+            .expect("Content-Type", /application\/json/)
+            .expect("Access-Control-Allow-Origin", "http://localhost:8080");
 
         expect(response.body).toEqual(planets);
     });
@@ -101,7 +102,8 @@ describe("POST /planets", () => {
             .post("/planets")
             .send({ name: "Mercury", diameter: 1234, moons: 12 })
             .expect(201)
-            .expect("Content-Type", /application\/json/);
+            .expect("Content-Type", /application\/json/)
+            .expect("Access-Control-Allow-Origin", "http://localhost:8080");
 
         expect(response.body).toEqual(planet);
     });
@@ -152,7 +154,8 @@ describe("PUT /planets/:id", () => {
                 moons: 12,
             })
             .expect(200)
-            .expect("Content-Type", /application\/json/);
+            .expect("Content-Type", /application\/json/)
+            .expect("Access-Control-Allow-Origin", "http://localhost:8080");
 
         expect(response.body).toEqual(planet);
     });
@@ -213,7 +216,10 @@ describe("PUT /planets/:id", () => {
 
 describe("DELETE /planets:id", () => {
     test("Valid request", async () => {
-        const response = await request.delete("/planets/1").expect(204);
+        const response = await request
+            .delete("/planets/1")
+            .expect(204)
+            .expect("Access-Control-Allow-Origin", "http://localhost:8080");
 
         expect(response.text).toEqual("");
     });
