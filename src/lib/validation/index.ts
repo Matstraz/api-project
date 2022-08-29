f8d023efcaa7e30f48069c596307496e57ebabf8
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2022-08-29 13:37:28 +0200

    Add validation for POST /planets route

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,30 @@
+import addFormats from "ajv-formats";
+import { Validator, ValidationError } from "express-json-validator-middleware";
+import { ErrorRequestHandler } from "express";
+
+const validator = new Validator({});
+
+addFormats(validator.ajv, ["date-time"])
+    .addKeyword("kind")
+    .addKeyword("modifier");
+
+export const validate = validator.validate;
+
+export const validationErrorMiddleware: ErrorRequestHandler = (
+    error,
+    request,
+    respose,
+    next
+) => {
+    if (error instanceof ValidationError) {
+        respose.status(422).send({
+            errors: error.validationErrors,
+        });
+
+        next();
+    } else {
+        next(error);
+    }
+};
+
+export * from "./planet";
